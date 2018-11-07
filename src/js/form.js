9a8b31d8448d97d96edeeb17f2f407612a96351b
brianotoole <botoole518@gmail.com>
2018-11-07 13:45:17 -0500

    add form

--- src/js/form.js
+++ src/js/form.js
@@ -1,45 +1,44 @@
 // validate on formsubmit
 var formSubmit = $('#js-form-submit');
 formSubmit.click(function() {
-	validateForm();
-	return false;
+    validateForm();
+    return false;
 });
 
 // form validation
 function validateForm() {
-	var email, atpos, dotpos, username;
+    var email, atpos, dotpos, username;
 
-	name = $('#name').val();
-	email = $('#email').val();
-	plan = $('#plan').val();
-	atpos = email.indexOf('@');
-	dotpos = email.lastIndexOf('.');
+    name = $('#name').val();
+    email = $('#email').val();
+    plan = $('#plan').val();
+    atpos = email.indexOf('@');
+    dotpos = email.lastIndexOf('.');
 
-	if (name == null || name == '') {
-	$('#js-form-message').addClass('text-error text-large').text('* Please enter your name.');
-	$('#name').focus();
-	return false;
-	}
-	if(email == null || email == '') {
-	$('#js-form-message').addClass('text-error text-large').text('* Please enter your email.');
-	$('#email').focus();
-	return false;
-	}
-	if(atpos < 1 || dotpos < atpos+2 || dotpos+2 >= email.length) {
-	$('#js-form-message').addClass('text-error text-large').text('* Please enter a valid email address.');
-	$('#email').focus();
-	return false;
-	}
-	else {
-	  var datastring = $("#js-form").serialize() + '&Plan=' + plan;
-	  $.ajax({
-	    //send the form using formspree
-	    url: "https://formcarry.com/s/gR62L1N7cgF", 
-	    method: "POST",
-	    data: datastring,
-	    dataType: "json"
-	  });
-	  $('#js-form-message').addClass('text-success text-large').text('Thanks for your interest! Your message has been sent successfully. We\'ll be in touch as soon as possible.');
-	  return false;
-	}
+    if (name == null || name == '') {
+        $('#js-form-message').addClass('text-error text-large').text('* Please enter your name.');
+        $('#name').focus();
+        return false;
+    }
+    if (email == null || email == '') {
+        $('#js-form-message').addClass('text-error text-large').text('* Please enter your email.');
+        $('#email').focus();
+        return false;
+    }
+    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length) {
+        $('#js-form-message').addClass('text-error text-large').text('* Please enter a valid email address.');
+        $('#email').focus();
+        return false;
+    } else {
+        var datastring = $("#js-form").serialize() + '&Plan=' + plan;
+        $.ajax({
+            //send the form using formspree
+            url: "https://formcarry.com/s/KUFCxYCBoPJ",
+            method: "POST",
+            data: datastring,
+            dataType: "json"
+        });
+        $('#js-form-message').addClass('text-success text-large').text('Thanks for your interest. We\'ll be in touch as soon as possible.');
+        return false;
+    }
 }
