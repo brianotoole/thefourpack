1c54ddda989892066eb43909582a2c3ed97681a6
brianotoole <botoole518@gmail.com>
2018-11-08 08:32:08 -0500

    update tweens

--- src/js/Tweens.js
+++ src/js/Tweens.js
@@ -16,9 +16,9 @@ var introItems = $('.section-intro > *');
 function slideInHeroItems() {
     heroItems.each(function() {
         tl.add(
-            TweenMax.from($(this), 0.3, {
+            TweenMax.from($(this), 0.15, {
                 autoAlpha: 0,
-                y: 0,
+                y: 5,
                 ease: Power0.easeOut
             })
         );
@@ -28,9 +28,9 @@ function slideInHeroItems() {
 function slideUpIntroItems() {
     introItems.each(function() {
         tl.add(
-            TweenMax.from($(this), 0.25, {
+            TweenMax.from($(this), 0.15, {
                 autoAlpha: 0,
-                y: 20,
+                y: 5,
                 ease: Power0.easeOut
             })
         );
@@ -47,7 +47,7 @@ function staggerInServices() {
         .setTween(tl1)
         //.addIndicators() //debug only
         .addTo(controller);
-    return tl1.staggerFrom(".service__item", 0.25, { x: -20, autoAlpha: 0, ease: Power1.easeOut }, 0.3);
+    return tl1.staggerFrom(".service__item", 0.25, { y: 5, autoAlpha: 0, ease: Power1.easeOut }, 0.3);
 }
 
 function slideUpFooter() {
@@ -60,7 +60,7 @@ function slideUpFooter() {
         .setTween(tl2)
         //.addIndicators() //debug only
         .addTo(controller);
-    return tl2.from(".btn-contact", 0.3, { x: 2, autoAlpha: 0, ease: Power0.easeOut }, 0.3);
+    return tl2.from(".btn-contact", 0.3, { y: 5, autoAlpha: 0, ease: Power0.easeOut }, 0.3);
 }
 
 
